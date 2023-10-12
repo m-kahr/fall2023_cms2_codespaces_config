771f72313eb6d37610f43f482c437ce9e4414c12
Michael Kahr <45453344+m-kahr@users.noreply.github.com>
2023-10-12 10:18:52 +0200

    uploaded install file

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#install vscode extensions
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-python.pylint
+
+#install python packages
+pip install faker
+pip install wikipedia
